23886dfb2bc659b39eb7e76b1bffb9f8f9af90ae
jayden <jayden.lester@berkeley.edu>
2026-04-09 23:03:56 -0700

    Added Chicken charger

--- Assets/Scripts/PlatformerScripts/EnemyScripts/EnemyCharge.cs
+++ Assets/Scripts/PlatformerScripts/EnemyScripts/EnemyCharge.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class EnemyCharge : EnemyMovement
 {
@@ -8,68 +9,153 @@ public class EnemyCharge : EnemyMovement
     //Accelerate speed
     // Add a disengage trigger
     // an alert queue for when enemy starts chasing 
-    [SerializeField] private EnemyMovement basicMovement;
-    private bool isChasing;
+    public Vector2 agrroRange = new Vector2(15f, 10f);
+    public Vector2 deagrroRange = new Vector2(15f, 20f);
     
-    public bool horizontalOnly = true;
-    public float chaseSpeed = 3f;
-    private float currentSpeed = 1f;
-    private Transform target;
-    public BoxCollider2D trigger;
-    protected Rigidbody2D rb;
+    
+    public float agroSpeed = 2f;
+    public float searchIntervals = 0.25f;
+
+    //public float knockbackTime = 0.15f;
+    //private bool stopMoving = false;
+
+
+
+    private bool isChasing = false;
+    public EnemyMovement idleScript; 
+    public LayerMask playerLayer;
+    private Transform player;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        basicMovement = GetComponent<EnemyMovement>();
+        sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        isChasing = false;
+        StartCoroutine(SearchForPlayer());
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
+    void Update()
     {
-        if (!isChasing || target == null)
-            return;
 
-        Vector2 direction = ((Vector2)target.position - rb.position).normalized;
+    }
 
-        if (horizontalOnly)
+    void FixedUpdate()
+    {
+        if (isChasing && player != null)
         {
-            direction = new Vector2(Mathf.Sign(direction.x), 0f);
-        }
-
-        rb.linearVelocity = new Vector2(direction.x * chaseSpeed, rb.linearVelocity.y);
-    }
-
-    //If player is 
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isChasing = true;
-            basicMovement.enabled = false;
-            target = collision.transform;
-        }
+            float distance = Mathf.Abs(rb.position.x - player.position.x);
         
-    }
+            if (distance <= 0.01) return;
 
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            target = null;
-            isChasing = false;
-            basicMovement.enabled = true;
+            Vector2 nextPosition;
+            
+
+            nextPosition.x = (player.position.x > rb.position.x ? 1f : -1f) * agroSpeed;
+            sr.flipX = player.position.x > rb.position.x ? true : false;
+
+            nextPosition.y = rb.linearVelocityY;
+            rb.linearVelocity = nextPosition;
+
+           // Vector2 nextPosition = Vector2.MoveTowards(
+           // rb.position, new Vector2(player.position.x, 0), agroSpeed * Time.fixedDeltaTime
+            //);
+
+            //rb.MovePosition(nextPosition);
         }
-        
     }
 
-
-    private IEnumerator Accelerate()
+    IEnumerator SearchForPlayer()
     {
-        
-        yield return null;
+        while(true){
+            //check to see if player is in range and to agro
+            while (!isChasing)
+            {
+                yield return new WaitForSeconds(searchIntervals);
+
+
+                Collider2D[] viewArea = Physics2D.OverlapBoxAll(transform.position, agrroRange, 0f, playerLayer);
+                foreach (Collider2D hit in viewArea) {
+                    if (hit.CompareTag("Player"))
+                    {
+                        Debug.Log("Player Found!");
+                        //TODO add a alert animation + sfx
+                        player = hit.transform;
+                        isChasing = true;
+                        //disable idle
+                        idleScript.enabled = false;
+                    }
+                }
+            }
+
+
+            //check if needed to de-argro
+            while (isChasing)
+            {
+                Collider2D[] viewArea = Physics2D.OverlapBoxAll(transform.position, deagrroRange, 0f, playerLayer);
+                //bool playerGone = true;
+                //player is not in view 
+                if (viewArea.Length == 0)
+                {
+                    player = null;
+                    isChasing = false;
+
+                    //if knockback is happening
+                    //CancelKnockback();
+
+                    idleScript.enabled = true;
+                }
+
+            
+                yield return new WaitForSeconds(searchIntervals);
+            }
+
+        }
     }
 
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, agrroRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, deagrroRange);
+
+    }
+
+
+
+
+   /* public virtual void applyKnockback(Vector2 hitFromPosition, float upwardForce = 2f, float knockbackForce = 8f)
+    {
+        CancelInvoke(nameof(EndKnockback));
+
+        stopMoving = true;
+
+         float xDir = transform.position.x > hitFromPosition.x ? 1f : -1f;
+
+        //set x velocity to 0 for smoother knockback
+        rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+
+        Vector2 force = new Vector2(xDir, upwardForce).normalized * knockbackForce;
+        rb.AddForce(force, ForceMode2D.Impulse);
+
+        //switch to end knockback when landing instead of timer(?)
+        Invoke(nameof(EndKnockback), knockbackTime);
+    }
+
+    public void CancelKnockback()
+{
+    CancelInvoke(nameof(EndKnockback));
+    stopMoving = false;
+    rb.linearVelocity = Vector2.zero;
 }
+
+    void EndKnockback()
+    {
+        stopMoving = false;
+    }*/
+}
+
